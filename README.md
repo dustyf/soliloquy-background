7788968f997758fb0187ad54a7f70fc237235b17
Dustin Filippini <dusty@dustyf.com>
2013-08-02 14:20:25 -0500

    Added initial code and updated Readme File

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-soliloquy-bg
+Soliloquy Background
 ============
 
-Allows Soliloquy Slider images to be output as the background of the element instead of an image.  This allows you to use images larger than the typical viewport.
+Allows Soliloquy Slider images to be output as the background of the element instead of an image.  This allows you to use images larger than the typical viewport as part of your slider.
+
+This plugin is Bring Your Own CSS.  Since Soliloquy typically resizes based upon your image sizes, you will have to set your own heights in the CSS For the containers and list items in Soliloquy.
---
+++ soliloquy-background.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Plugin Name: Soliloquy Background
+Plugin URI: http://dustyf.com/
+Description: Output slider as background images
+Author: Dustin Filippini
+Author URI: http://dustyf.com/
+Version: 0.1
+License: GNU General Public License v2.0 or later
+License URI: http://www.opensource.org/licenses/gpl-license.php
+*/
+
+/*
+	Copyright 2013	 Dustin Filippini	 (email : dusty@dustyf.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// add_filter('tgmsp_image_output', '<img class="soliloquy-item-image" src="' . esc_url( $image['src'] ) . '" alt="' . esc_attr( $alt ) . '" title="' . esc_attr( $title ) . '" />', $id, $image, $alt, $title );
+
+add_filter('tgmsp_slider_item_style', 'add_soliloquy_bg_image', 10, 4);
+function add_soliloquy_bg_image($style, $id, $image, $i) {
+	$style = 'background-image:url(' . $image['src'] . ');';
+	var_dump($image);
+	return $style;
+}
+
+add_filter('tgmsp_image_output', 'remove_soliloquy_slide_image', 10, 5);
+function remove_soliloquy_slide_image($imagehtml, $id, $image, $alt, $title) {
+	$imagehtml = '';
+	return $imagehtml;
+}
